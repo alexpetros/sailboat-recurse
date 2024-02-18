86e01277237aabaeb4f137355a4a0b24dceb8cbe
Alexander Petros <apetros15@gmail.com>
2024-02-18 14:35:45 -0500

    Parse address

--- src/main.rs
+++ src/main.rs
@@ -18,12 +18,12 @@ const PORT: u16 = 3000;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let addr = SocketAddr::from(([127, 0, 0, 1], PORT));
+    let addr:SocketAddr = format!("0.0.0.0:{}", PORT).parse()?;
 
     // We create a TcpListener and bind it to 127.0.0.1:3000
     let listener = TcpListener::bind(addr).await?;
 
-    println!("Now listening at http://localhost:{:?}", PORT);
+    println!("Now listening at http://localhost:{}", PORT);
     // We start a loop to continuously accept incoming connections
     loop {
         let (stream, _) = listener.accept().await?;
@@ -40,7 +40,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
                 .serve_connection(io, service_fn(hello))
                 .await
             {
-                println!("Error serving connection: {:?}", err);
+                println!("Error serving connection: {}", err);
             }
         });
     }
