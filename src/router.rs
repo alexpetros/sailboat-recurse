158824b0fee3c1ded44d96f58def1041fb15ca50
Alexander Petros <apetros15@gmail.com>
2024-03-05 18:28:41 -0500

    Remove echo stuff

--- src/router.rs
+++ src/router.rs
@@ -1,6 +1,5 @@
 mod index;
 mod serve_static;
-mod echo;
 
 use std::sync::Arc;
 use http_body_util::combinators::BoxBody;
@@ -9,14 +8,11 @@ use hyper::Method;
 use hyper::{Request, Response};
 use tracing::debug;
 
-use crate::router::echo::echo;
-use crate::router::echo::echo_upper;
-use crate::router::echo::echo_reversed;
 use crate::request_utils;
 use crate::GlobalContext;
 
 const GET: &Method = &Method::GET;
-const POST: &Method = &Method::POST;
+// const POST: &Method = &Method::POST;
 
 pub async fn router(
     req: Request<hyper::body::Incoming>,
@@ -34,9 +30,6 @@ pub async fn router(
     match (method, path) {
         (GET, "/healthcheck") => Ok(request_utils::send("OK")),
         (GET, "/") => index::get(req, g_ctx),
-        (POST, "/echo") => echo(req),
-        (POST, "/echo/uppercase") => echo_upper(req),
-        (POST, "/echo/reversed") => echo_reversed(req).await,
 
         // Return 404 otherwise
         _ => Ok(request_utils::not_found())
