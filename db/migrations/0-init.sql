f8f8ac9e6b2b21cc186c0314112766c7338a1b21
Alexander Petros <apetros15@gmail.com>
2024-03-06 16:11:27 -0500

    Make initial db schema

---
+++ db/migrations/0-init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE posts (
+  author_name TEXT,
+  author_handle TEXT,
+  content TEXT
+);
+
+INSERT INTO posts (author_name, author_handle, content)
+VALUES
+('Alex', 'awp@exmaple.com', 'This is my first sailboat post!')
+;
