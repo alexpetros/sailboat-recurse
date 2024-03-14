7c0de4ab7bd4fce62a8afb9d59e74177334615e1
Alexander Petros <apetros15@gmail.com>
2024-03-14 19:30:35 -0400

    Add htmx

--- src/router/serve_static.rs
+++ src/router/serve_static.rs
@@ -6,9 +6,12 @@ use crate::server::response;
 
 pub async fn get(req: Request, ctx: Context<'_>) -> ResponseResult {
     let path = req.uri().path();
+    if path.len() <= 8 {
+        return response::not_found(req, ctx).await;
+    }
     let file = &path[8..];
-    let contents = ctx.global.statics.get(file);
 
+    let contents = ctx.global.statics.get(file);
     match contents {
         Some(body) => response::send_async(body.clone()).await,
         None => response::not_found(req, ctx).await
