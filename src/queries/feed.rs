408ccc95531a3a527204586a736a5d8d61cd196f
Alexander Petros <apetros15@gmail.com>
2024-03-19 17:38:47 -0400

    Add timestamp

--- src/queries/feed.rs
+++ src/queries/feed.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Local};
 use rusqlite::Connection;
 use serde::Serialize;
 use crate::server::error::ServerError;
@@ -7,23 +8,34 @@ pub struct Post {
     post_id: i64,
     display_name: String,
     handle: String,
-    content: String
+    content: String,
+    created_at: String
 }
 
 pub fn get_posts_in_feed (db: &Connection, feed_id: i64) -> Result<Vec<Post>, ServerError> {
     let mut query = db.prepare(
-        "SELECT post_id, display_name, handle, content
+        "SELECT post_id,
+            display_name,
+            handle,
+            content,
+            created_at
          FROM posts
          LEFT JOIN feeds AS f USING (feed_id)
          WHERE feed_id = ?1
-         ORDER BY created_at ASC
+         ORDER BY created_at DESC
          ")?;
     let rows = query.query_map([feed_id], |row| {
+        // TODO: User-local timestamp handling
+        let created_at = DateTime::from_timestamp(row.get(4)?, 0)
+            .unwrap()
+            .with_timezone(&Local);
+        let created_at = created_at.format("%b %m %Y, %r").to_string();
         let post = Post {
             post_id: row.get(0)?,
             display_name: row.get(1)?,
             handle: row.get(2)?,
-            content: row.get(3)?
+            content: row.get(3)?,
+            created_at
         };
         Ok(post)
     })?;
--- src/router/posts.rs
+++ src/router/posts.rs
@@ -33,7 +33,7 @@ pub async fn post(req: Request, ctx: Context<'_>) -> ResponseResult {
     )?;
 
     let posts = get_posts_in_feed(&ctx.db, feed_id)?;
-    let body = ctx.render_block("index.html", "feed", context! { posts });
+    let body = ctx.render_block("feed.html", "feed", context! { posts });
     Ok(send(body))
 }
 
